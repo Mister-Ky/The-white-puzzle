1d01f0ac1974a52bf2a71f3b99b0a72c9d85073e
Mister Ky <afanasievsergiy3524@gmail.com>
2026-02-11 17:02:08 +0200

    update 1.1.4.0
    fix _physics_process to _process.
    add lerp in camera.gd.
    delete 2 unused scripts.
    optimize.

--- scripts/TimeLabel.gd
+++ scripts/TimeLabel.gd
@@ -6,22 +6,22 @@ var is_running := false
 
 func _ready() -> void:
 	_set_time_text(elapsed_time)
-	set_physics_process(is_running)
+	set_process(is_running)
 
 func _set_time_text(time : float) -> void:
 	text = "Time: " + String("%0.2f" % time) + " sec"
 
 func start() -> void:
 	is_running = true
-	set_physics_process(is_running)
+	set_process(is_running)
 
 func stop() -> void:
 	is_running = false
-	set_physics_process(is_running)
+	set_process(is_running)
 
 func reset() -> void:
 	elapsed_time = 0.0
 
-func _physics_process(delta : float) -> void:
+func _process(delta : float) -> void:
 	elapsed_time += delta
 	_set_time_text(elapsed_time)
